3cf39e0f8f32fc450441ac4df86649c4aae9d96e
Kevin Kirby <kkirby16@my.whitworth.edu>
2021-07-01 07:40:09 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,67 @@
+# run_code_inside = false
+# puts "Code before if...end"
+# if run_code_inside
+#  puts "code inside"
+# end
+# puts "Code after if...end" 
+
+# chance_of_rain = 1
+# puts "Let's go outside!"
+# if chance_of_rain > 0.5
+  # puts "Pack an umbrella!"
+# else
+  # puts "Enjoy the fine day!"
+# end
+# puts "Oh, and always wear sunscreen!"
+
+#chance_of_rain = -23	
+#if chance_of_rain <= 0.25
+  #puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  #puts "Pack an umbrella!"
+#else
+  #puts "Stay home and read Hegel."
+#end
+
+#puts "You know what year it is??"
+#this_year = 2019
+#puts "Hey, it's 2019!" if 
+#this_year == 2019 
+
+#this_year = 2020
+#puts "Hey, it's not 2019!"
+#unless this_year == 2019
+
+# greeting = "friendly_greeting"
+ 
+#case greeting
+ # when "unfriendly_greeting"
+  #  puts "What do you want!?"
+  #when "friendly_greeting"
+   # puts "Hi! How are you?"
+# end
+
+#magic_exit_number = 40
+#count = 0
+#while count < 50 && count != magic_exit_number do 
+ # puts "I am this number now: #{count}"
+  #count*= 1
+#end
+
+#counter = 0 
+#until counter == 20 
+#puts "The current number is less than 20."
+ #counter+= 1
+ #end 
+
+def 
+say_hello_world_five_times
+  message = "Hello World!"
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+say_hello_world_five_times
